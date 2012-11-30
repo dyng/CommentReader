92fdd260cd2810dbc9b5d08639a07336c80bf14f
gvirus <dygvirus@gmail.com>
2012-11-30 22:14:56 +0900

    add weibo support

--- plugin/commentreader.py
+++ plugin/commentreader.py
@@ -1,4 +1,9 @@
 import vim
+import urllib
+import urllib2
+from datetime import datetime
+import json
+import logging
 
 # global variables
 langdict = {
@@ -9,36 +14,54 @@ langdict = {
             'cpp':     { 'prefix':  '//', 'filler':  '//', 'suffix':  '//', 'defs':  r''},
            }
 
-class Init():
+class CommentReader():
     def __init__(self):
         self.option  = {
-                'line_len' : int(vim.eval('g:creader_lines_per_block')),
-                'line_num' : int(vim.eval('g:creader_chars_per_line')),
+                'line_len': int(vim.eval('g:creader_lines_per_block')),
+                'line_num': int(vim.eval('g:creader_chars_per_line')),
+                }
+        self.meta = {
+                'debug_mode': int(vim.eval('g:creader_debug_mode')),
+                'debug_file': vim.eval('g:creader_debug_file'),
+                'app_key':    '1861844333',
+                'app_secret': '160fcb0ca75b22e35c644f8758e279c1',
                 }
         self.content = None
         self.view    = View(self.option)
         self.pageNum = 0
         self.sectNum = 0
 
+        # logging setting
+        if self.meta['debug_mode'] == 1:
+            logging.basicConfig(filename=self.meta['debug_file'], level=logging.DEBUG, format='%(asctime)s %(message)s')
+
 
     # opens
 
     def openBook(self, path):
-        self.content = Content('book', Book(path, self.option), self.option)
+        self.content = Content('book', Book(path, self.meta, self.option), self.option)
         self.view.refreshAnchor()
         self.show()
 
-    def openWeibo(self):
-        pass
+    def openWeibo(self, auth_code):
+        self.content = Content('weibo', Weibo(auth_code, self.meta, self.option), self.option)
+        self.view.refreshAnchor()
+        self.show()
 
     def openTwitter(self):
         pass
 
+    def refresh(self):
+        self.content.refresh()
+        self.pageNum = 0
+        self.sectNum = 0
+        self.show()
 
     # closes
 
     def close(self):
         pass
+
 
 
     # views
@@ -48,7 +71,7 @@ class Init():
         self.view.clear()
 
         # get content and render 
-        raw_content_list = self.content.readContent(self.pageNum, self.view.getAnchorNum())
+        raw_content_list = self.content.read(self.pageNum, self.view.getAnchorNum())
         content_list = self.view.commentize_list(raw_content_list)
         self.view.render(content_list)
 
@@ -56,7 +79,10 @@ class Init():
         self.first()
 
     def hide(self):
+        # save and restore cursor position
+        (line_bak, col_bak) = (vim.eval("line('.')"), vim.eval("col('.')"))
         self.view.clear()
+        vim.command("call cursor('{0}', '{1}')".format(line_bak, col_bak))
 
 
     # cursor moves
@@ -98,25 +124,25 @@ class Init():
         else:
             self.view.pointTo(self.sectNum)
 
-
 class Anchor():
     def __init__(self, rel_posi, pre_anchor):
-        self.relPosi   = rel_posi
-        self.absPosi   = -1
-        self.size      = 0
-        self.preAnchor = pre_anchor
+        self.rel_posi   = rel_posi
+        self.abs_posi   = -1
+        self.size       = 0
+        self.pre_anchor = pre_anchor
 
     def evalAbsPosition(self):
-        if self.preAnchor is None:
-            self.absPosi = self.relPosi
-            return self.absPosi
+        if self.pre_anchor is None:
+            self.abs_posi = self.rel_posi
+            return self.abs_posi
         else:
-            self.absPosi = self.preAnchor.evalAbsPosition() + self.preAnchor.size + self.relPosi
-            return self.absPosi
+            self.abs_posi = self.pre_anchor.evalAbsPosition() + self.pre_anchor.size + self.rel_posi
+            return self.abs_posi
 
+    # Warning: if you need EXACT position, call evalAbsPosition instead of this
     def getAbsPosition(self):
-        if self.absPosi != -1:
-            return self.absPosi
+        if self.abs_posi != -1:
+            return self.abs_posi
         else:
             return self.evalAbsPosition()
 
@@ -142,7 +168,7 @@ class View():
             self.filler = langdict[filetype]['filler']
             self.suffix = langdict[filetype]['suffix']
         else:
-            vim.command("echo 'Sorry, this language is not supported.'")
+            vim.command("echom 'Sorry, this language is not supported.'")
 
         # define declaration reserved word
         self.defs = langdict[filetype]['defs']
@@ -152,7 +178,7 @@ class View():
 
         self.refreshAnchor()
         if self.getAnchorNum() == 0:
-            vim.command("echo 'Sorry, there is no place for comment.'")
+            vim.command("echom 'Sorry, there is no place for comment.'")
 
 
     # anchors
@@ -251,14 +277,19 @@ class Content():
         self.type   = type
         self.stream = stream
 
-    def readContent(self, index, amount):
+    def read(self, index, amount):
         items = self.stream.getItem(index, amount)
-        return [i.content for i in items]
+        return [i.content() for i in items]
+
+    def refresh(self):
+        if self.type == 'weibo':
+            stream.refreshWeibo()
+
 
 # Content: Book
 
 class Book():
-    def __init__(self, path, option):
+    def __init__(self, path, meta, option):
         self.fd      = open(path, 'r')
         self.items   = []
         self.lineLen = option['line_len']
@@ -298,9 +329,92 @@ class Page():
                 line_loaded += 1
                 p += self.lineNum
 
-        self.content = "\n".join(content)
+        self.string = "\n".join(content)
+
+    def content(self):
+        return self.string
 
 # Content: Weibo
+
+class Weibo():
+    def __init__(self, auth_code, meta, option):
+        self.auth_code = auth_code
+        self.items = []
+        self.next_page = 1
+        self.token_info = None
+
+    def reqAccessToken(self):
+        client_id = '1861844333'
+        client_secret = '160fcb0ca75b22e35c644f8758e279c1'
+        redirect_uri = 'https://api.weibo.com/oauth2/default.html'
+
+        url = 'https://api.weibo.com/oauth2/access_token'
+        params = {
+                'client_id': client_id,
+                'client_secret': client_secret,
+                'grant_type': 'authorization_code',
+                'redirect_uri': redirect_uri,
+                'code': self.auth_code,
+                }
+
+        try:
+            logging.debug("request: " + url + ' POST: ' + urllib.urlencode(params))
+            res = urllib2.urlopen(url, urllib.urlencode(params))
+            self.token_info = json.load(res)
+            logging.debug("token_info: " + str(self.token_info))
+        except:
+            # TODO: error handle
+            logging.exception('')
+
+        return self.token_info['access_token']
+
+    # you can get more tweets by calling this method repeatedly
+    def pullTweets(self):
+        # call reqAccessToken first if access_token not defined
+        if self.token_info is None:
+            self.reqAccessToken()
+
+        url = 'https://api.weibo.com/2/statuses/home_timeline.json'
+        params = {
+                'access_token': self.token_info['access_token'],
+                'count': 20,
+                'page': self.next_page,
+                }
+
+        try:
+            logging.debug("request: " + url + '?' + urllib.urlencode(params))
+            res = urllib2.urlopen(url + '?' + urllib.urlencode(params))
+            raw_timeline = json.load(res)
+            logging.debug("timeline res: " + str(raw_timeline))
+            self.next_page += 1
+        except:
+            # TODO: error handle
+            logging.exception('')
+
+        for raw_tweet in raw_timeline['statuses']:
+            # we need only tweets that older than the tail tweet
+            if int(raw_tweet['id']) >= self.items[-1].id: continue
+            self.items.append(Tweet(raw_tweet))
+
+        return self.items
+
+    def getItem(self, index, amount):
+        while index + amount > len(self.items):
+            self.pullTweets()
+        return self.items[index:index+amount]
+
+    def refreshWeibo(self):
+        self.items = []
+        self.next_page = 1
+
+class Tweet():
+    def __init__(self, raw_tweet):
+        self.id     = int(raw_tweet['id'])
+        self.author = raw_tweet['user']['screen_name']
+        self.text   = raw_tweet['text']
+
+    def content(self):
+        return self.author + ": " + self.text + "\n"
 
 # Content: Douban
 
--- plugin/commentreader.vim
+++ plugin/commentreader.vim
@@ -14,13 +14,31 @@ endif
 if !exists('g:creader_lines_per_block')
     let g:creader_lines_per_block = 5
 endif
+if !exists('g:creader_debug_mode')
+    let g:creader_debug_mode = 1
+endif
+if !exists('g:creader_debug_file')
+    let g:creader_debug_file = '/var/tmp/creader.log'
+endif
 
 " initialization
-python cr_instance = Init()
+python cr_instance = CommentReader()
 
 " define functions
 function! s:CRopenbook(path)
     python cr_instance.openBook(vim.eval('a:path'))
+endfunction
+
+function! s:CRopenweibo(auth_code)
+    python cr_instance.openWeibo(vim.eval('a:auth_code'))
+endfunction
+
+function! s:CRopendouban()
+    python cr_instance.openDouban()
+endfunction
+
+function! s:CRopentwitter()
+    python cr_instance.openTwitter()
 endfunction
 
 function! s:CRhide()
@@ -44,12 +62,13 @@ function! s:CRprevious()
 endfunction
 
 " define commands
-command! -nargs=1 -complete=file CRopenbook call s:CRopenbook('<args>')
-command! -nargs=0                CRforward  call s:CRforward()
-command! -nargs=0                CRbackward call s:CRbackward()
-command! -nargs=0                CRhide     call s:CRhide()
-command! -nargs=0                CRnext     call s:CRnext()
-command! -nargs=0                CRprevious call s:CRprevious()
+command! -nargs=1 -complete=file CRopenbook  call s:CRopenbook('<args>')
+command! -nargs=1                CRopenweibo call s:CRopenweibo('<args>')
+command! -nargs=0                CRforward   call s:CRforward()
+command! -nargs=0                CRbackward  call s:CRbackward()
+command! -nargs=0                CRhide      call s:CRhide()
+command! -nargs=0                CRnext      call s:CRnext()
+command! -nargs=0                CRprevious  call s:CRprevious()
 
 " define maps
 nnoremap <silent> <leader>d :CRforward<CR>
